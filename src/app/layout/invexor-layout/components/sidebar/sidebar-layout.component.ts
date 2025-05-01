38c28ba404d108ed4f0cf6a6bafd6612935b11b3
JhustynC <christopherarias2002@gmail.com>
2025-05-01 01:18:50 -0500

    feat: toggle theme button

--- src/app/layout/invexor-layout/components/sidebar/sidebar-layout.component.ts
+++ src/app/layout/invexor-layout/components/sidebar/sidebar-layout.component.ts
@@ -14,6 +14,7 @@ import {
 export class SidebarLayoutComponent {
   @ViewChild('sidebar') sidebar!: ElementRef<HTMLElement>;
   @ViewChild('toggleBtn') toggleButton!: ElementRef<HTMLButtonElement>;
+  @ViewChild('#toggleTheme') toggleTheme!: ElementRef<HTMLButtonElement>;
 
   // Método alternativo usando HostListener
   @HostListener('window:resize', ['$event'])
