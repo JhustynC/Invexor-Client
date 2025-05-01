ee9a9fd238a91c6a6a17b4043cb6a5381762c69f
JhustynC <christopherarias2002@gmail.com>
2025-05-01 00:47:56 -0500

    fix: responsive sidebar fix

--- src/app/layout/invexor-layout/components/sidebar/sidebar-layout.component.ts
+++ src/app/layout/invexor-layout/components/sidebar/sidebar-layout.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'sidebar-layout',
@@ -6,5 +12,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar-layout.component.css'],
 })
 export class SidebarLayoutComponent {
+  @ViewChild('sidebar') sidebar!: ElementRef<HTMLElement>;
+  @ViewChild('toggleBtn') toggleButton!: ElementRef<HTMLButtonElement>;
 
+  // Método alternativo usando HostListener
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    if (
+      window.innerWidth <= 800 &&
+      this.sidebar?.nativeElement.classList.contains('close')
+    ) {
+      this.sidebar?.nativeElement.classList.toggle('close');
+      this.toggleButton?.nativeElement.classList.toggle('rotate');
+    }
+  }
+
+  public toggleSidebar() {
+    this.sidebar?.nativeElement.classList.toggle('close');
+    this.toggleButton?.nativeElement.classList.toggle('rotate');
+    this.closeAllSubMenus();
+  }
+
+  public toggleSubMenu(button: HTMLElement) {
+    if (!button.nextElementSibling?.classList.contains('show')) {
+      this.closeAllSubMenus();
+    }
+
+    button.nextElementSibling?.classList.toggle('show');
+    button.classList.toggle('rotate');
+
+    if (this.sidebar?.nativeElement.classList.contains('close')) {
+      this.sidebar?.nativeElement.classList.toggle('close');
+      this.toggleButton?.nativeElement.classList.toggle('rotate');
+    }
+  }
+
+  public closeAllSubMenus() {
+    Array.from(
+      this.sidebar!.nativeElement.getElementsByClassName('show')
+    ).forEach((ul) => {
+      ul.classList.remove('show');
+      ul.previousElementSibling!.classList.remove('rotate');
+    });
+  }
 }
