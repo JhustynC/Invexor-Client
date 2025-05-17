a90ef1c8d4f1a4b0d6114115d6481f60e5d722cf
LeandroFreireGH <leandro.freire@ucuenca.edu.ec>
2025-05-17 11:04:49 -0500

    LF-ADD-Top categories bar chart

--- src/app/feature/dashboard/pages/grid-widgets/grid-widgets.component.ts
+++ src/app/feature/dashboard/pages/grid-widgets/grid-widgets.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, HostListener } from '@angular/core';
 import { TransactionChartComponent } from '../../components/transaction-chart/transaction-chart.component';
 import { TransactionsGraphComponent } from '../../components/transactions-graph/transactions-graph.component';
+import { TopCategoriesComponent } from '../../components/top-categories/top-categories.component';
 import {
   CompactType,
   DisplayGrid,
@@ -30,7 +31,8 @@ interface ItemByType extends GridsterItem {
   imports: [GridsterComponent,
     GridsterItemComponent,
     TransactionChartComponent,
-    TransactionsGraphComponent
+    TransactionsGraphComponent,
+    TopCategoriesComponent
   ],
   templateUrl: './grid-widgets.component.html',
   styles: `
@@ -123,7 +125,7 @@ export default class GridLayoutComponent {
     this.dashboard = [
       { cols: 2, rows: 1, y: 0, x: 0, type: 'Graph'},
       { cols: 2, rows: 2, y: 0, x: 2, hasContent: true, type: 'LastTransactions'},
-      { cols: 1, rows: 1, y: 0, x: 4},
+      { cols: 1, rows: 1, y: 0, x: 4, type: 'TopCategories'},
       { cols: 1, rows: 1, y: 2, x: 5},
       { cols: 1, rows: 1, y: 1, x: 0},
       { cols: 1, rows: 1, y: 1, x: 0}
