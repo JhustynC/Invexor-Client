491ebfce1b895cfb65e550e6b851eeb275d6e897
LeandroFreireGH <leandro.freire@ucuenca.edu.ec>
2025-05-18 11:25:10 -0500

    LF-ADD-Show information of the selected row in a table to edit, setting in the form form of addition

--- src/app/feature/entities-manager/pages/areas/areas.component.ts
+++ src/app/feature/entities-manager/pages/areas/areas.component.ts
@@ -1,16 +1,15 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  OnChanges,
   signal,
-  SimpleChanges,
 } from '@angular/core';
 import { JsonFormComponent } from '../../../../shared/components/json-form/json-form.component';
 import { TableCompositionComponent } from '../../../../shared/components/table-composition/table-composition.component';
+import { UpgratedFormComponent } from '../../../../shared/components/upgrated-form/upgrated-form.component';
 
 @Component({
   selector: 'areas-entites-manager',
-  imports: [JsonFormComponent, TableCompositionComponent],
+  imports: [TableCompositionComponent, UpgratedFormComponent],
   templateUrl: './areas.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -54,126 +53,4 @@ export default class AreasComponent {
     this.areas = [...this.areas, event];
     this.togglePopup();
   }
-  /* sucursales = [
-    {
-      nombre: 'Sucursal A',
-      ciudad: 'Cuenca',
-      telefono: '07-1234567',
-      estado: 'Activa',
-    },
-    {
-      nombre: 'Sucursal B',
-      ciudad: 'Quito',
-      telefono: '02-7654321',
-      estado: 'Inactiva',
-    },
-    {
-      nombre: 'Sucursal C',
-      ciudad: 'Guayaquil',
-      telefono: '04-9876543',
-      estado: 'Activa',
-    },
-    {
-      nombre: 'Sucursal D',
-      ciudad: 'Loja',
-      telefono: '07-1112233',
-      estado: 'Activa',
-    },
-    {
-      nombre: 'Sucursal E',
-      ciudad: 'Ambato',
-      telefono: '03-3344556',
-      estado: 'Inactiva',
-    },
-    {
-      nombre: 'Sucursal F',
-      ciudad: 'Riobamba',
-      telefono: '03-9988776',
-      estado: 'Activa',
-    },
-    {
-      nombre: 'Sucursal G',
-      ciudad: 'Manta',
-      telefono: '05-2233445',
-      estado: 'Activa',
-    },
-    {
-      nombre: 'Sucursal H',
-      ciudad: 'Esmeraldas',
-      telefono: '06-6677889',
-      estado: 'Inactiva',
-    },
-    {
-      nombre: 'Sucursal I',
-      ciudad: 'Tena',
-      telefono: '06-5544332',
-      estado: 'Activa',
-    },
-    {
-      nombre: 'Sucursal J',
-      ciudad: 'Ibarra',
-      telefono: '06-3344556',
-      estado: 'Activa',
-    },
-    {
-      nombre: 'Sucursal A',
-      ciudad: 'Cuenca',
-      telefono: '07-1234566',
-      estado: 'Activa',
-    },
-    {
-      nombre: 'Sucursal B',
-      ciudad: 'Quito',
-      telefono: '02-7654321',
-      estado: 'Inactiva',
-    },
-    {
-      nombre: 'Sucursal C',
-      ciudad: 'Guayaquil',
-      telefono: '04-9876543',
-      estado: 'Activa',
-    },
-    {
-      nombre: 'Sucursal D',
-      ciudad: 'Loja',
-      telefono: '07-1112233',
-      estado: 'Activa',
-    },
-    {
-      nombre: 'Sucursal E',
-      ciudad: 'Ambato',
-      telefono: '03-3344556',
-      estado: 'Inactiva',
-    },
-    {
-      nombre: 'Sucursal F',
-      ciudad: 'Riobamba',
-      telefono: '03-9988776',
-      estado: 'Activa',
-    },
-    {
-      nombre: 'Sucursal G',
-      ciudad: 'Manta',
-      telefono: '05-2233445',
-      estado: 'Activa',
-    },
-    {
-      nombre: 'Sucursal H',
-      ciudad: 'Esmeraldas',
-      telefono: '06-6677889',
-      estado: 'Inactiva',
-    },
-    {
-      nombre: 'Sucursal I',
-      ciudad: 'Tena',
-      telefono: '06-5544332',
-      estado: 'Activa',
-    },
-    {
-      nombre: 'Sucursal J',
-      ciudad: 'Ibarra',
-      telefono: '06-3344556',
-      estado: 'Activa',
-    },
-  ]; */
 }
---
+++ src/app/shared/components/upgrated-form/upgrated-form.component.ts
@@ -0,0 +1,182 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  AfterViewInit,
+  effect,
+  inject,
+  signal,
+  output,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { JsonFormControl, JsonFormData } from '../../interfaces/form.interface';
+import { input } from '@angular/core';
+import { FetchJsonService } from '../json-form/fetch-json.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'upgrated-form',
+  templateUrl: './upgrated-form.component.html',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, FormsModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./upgrated-form.component.css'],
+})
+export class UpgratedFormComponent implements AfterViewInit {
+  //! Usar este formilario para toda la aplicación
+
+  private formBuilder = inject(FormBuilder);
+  private fetchJsonService = inject(FetchJsonService);
+
+  jsonFormDataUrl = input<string>();
+  jsonFormData = signal<JsonFormData>({ controls: [] });
+  myForm: FormGroup = this.formBuilder.group({});
+  editableData = input<any>(undefined);
+
+  ngAfterViewInit() {
+    const url = this.jsonFormDataUrl();
+    if (url) {
+      this.fetchJsonService.fetchJsonData(url).subscribe({
+        next: (data) => this.jsonFormData.set(data),
+        error: (err) => console.error('Error cargando JSON:', err),
+      });
+    }
+  }
+
+  constructor() {
+    effect(() => {
+      this.createForm(this.jsonFormData().controls);
+    });
+  }
+
+  createForm(controls: JsonFormControl[]) {
+    const group: Record<string, any> = {};
+
+    for (const control of controls) {
+      const validatorsToAdd = [];
+/* 
+      if (control.type === 'select') {
+        
+        control.selectOptions = control.selectOptions?.map((option) => ({
+          ...option,
+          selected: option.label === this.editableData()?.[control.name]
+        }));
+        console.log(control.selectOptions);
+      }
+      else { */
+        
+        control.value = this.editableData()?.[control.name] ?? control.value;
+      //}
+
+      for (const [key, value] of Object.entries(control.validators ?? {})) {
+        switch (key) {
+          case 'min':
+            validatorsToAdd.push(Validators.min(value));
+            break;
+          case 'max':
+            validatorsToAdd.push(Validators.max(value));
+            break;
+          case 'minLength':
+            validatorsToAdd.push(Validators.minLength(value));
+            break;
+          case 'maxLength':
+            validatorsToAdd.push(Validators.maxLength(value));
+            break;
+          case 'pattern':
+            validatorsToAdd.push(Validators.pattern(value));
+            break;
+          case 'nullValidator':
+            if (value) validatorsToAdd.push(Validators.nullValidator);
+            break;
+          case 'required':
+            if (value) validatorsToAdd.push(Validators.required);
+            break;
+          case 'requiredTrue':
+            if (value) validatorsToAdd.push(Validators.requiredTrue);
+            break;
+          case 'email':
+            if (value) validatorsToAdd.push(Validators.email);
+            break;
+          // No se recomienda usar compose directamente desde JSON
+        }
+      }
+
+      group[control.name] = this.formBuilder.control(
+        control.value ?? '',
+        validatorsToAdd
+      );
+    }
+
+    this.myForm = this.formBuilder.group(group);
+  }
+
+  //! Para obtener los valores del formulario
+  submitFormValues = output<any>();
+
+  onSubmit() {
+    console.log('Form valid:', this.myForm.valid);
+    console.log('Form values:', this.myForm.value);
+    this.submitFormValues.emit(this.myForm.value);
+  }
+
+  //* Para añadir un nuevo campo al formulario
+  //TODO: Logica para guardar el formulario personalizado en cada branch (sucursal) y que se muestre al usuario una lista de los custom forms dependiendo de las sección
+  //TODO: Agregar custom selects al formulario - se puede hacer despues de la inicialización del formulario en el constructor
+  newFieldLabel = '';
+  newFieldOptions: string = ''; // separado por coma, por ejemplo: "Opción 1, Opción 2"
+  newFieldType: 'text' | 'number' | 'date' | 'select' = 'text';
+
+  addField() {
+    if (!this.newFieldLabel.trim()) return;
+
+    
+    const newControl: JsonFormControl = {
+      name: this.newFieldLabel,
+      label: this.newFieldLabel,
+      type: this.newFieldType,
+      value: '',
+      validators: { required: true },
+      selectOptions: [],
+    };
+
+    // Si es tipo SELECT, parseamos las opciones
+    if (this.newFieldType === 'select') {
+      newControl.selectOptions = this.newFieldOptions
+        .split(',')
+        .map((option) => {
+          const [value, label] = option.split(':');
+          return { value: value.trim(), label: label.trim() };
+        });
+    }
+
+    //? Solución - No volver a generar todos los controles desde 0
+    this.jsonFormData().controls.push(newControl);
+
+    //TODO: Guardar en BD en la branch especifica sin modificar el json original
+
+    this.myForm.addControl(
+      // newControlName
+      this.newFieldLabel,
+      this.formBuilder.control('', Validators.required)
+    );
+
+    this.newFieldLabel = '';
+    this.newFieldType = 'text';
+    this.newFieldOptions = '';
+
+    //TODO: Guardar en BD en la branch especifica sin modificar el json original
+  }
+
+  //? To cancel button
+  showCancelButton = input<boolean>(false);
+  cancelButtonOutput = output<void>();
+
+  onCancel() {
+    this.cancelButtonOutput.emit();
+  }
+}
