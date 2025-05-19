11cf9c03414a3aff9f2a77dae47c5d5079412bb3
JhustynC <christopherarias2002@gmail.com>
2025-05-18 19:37:56 -0500

    fix: users-form-exte

--- src/app/feature/entities-manager/pages/resources/resources.component.ts
+++ src/app/feature/entities-manager/pages/resources/resources.component.ts
@@ -1,18 +1,17 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { DynamicJsonFormComponent } from "../../../../shared/components/dynamic-json-form/dynamic-json-form.component";
-import { EnterImgComponent } from "../../../../shared/components/enter-img/enter-img.component";
-import { SubListComponent } from "../../../../shared/components/sub-list/sub-list.component";
+import { DynamicJsonFormComponent } from '../../../../shared/components/dynamic-json-form/dynamic-json-form.component';
+import { EnterImgComponent } from '../../../../shared/components/enter-img/enter-img.component';
+import { SubListComponent } from '../../../../shared/components/sub-list/sub-list.component';
 import { TableCompositionComponent } from '../../../../shared/components/table-composition/table-composition.component';
 import { UpgratedFormComponent } from '../../../../shared/components/upgrated-form/upgrated-form.component';
 
 @Component({
   selector: 'app-resources',
-  imports: [EnterImgComponent, SubListComponent, TableCompositionComponent, UpgratedFormComponent],
+  imports: [SubListComponent, TableCompositionComponent, UpgratedFormComponent],
   templateUrl: './resources.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class ResourcesComponent {
-
   openPopup = signal<boolean>(false);
   formData = signal<any>(undefined);
   selectedTableResource = signal<any>(undefined);
@@ -23,22 +22,22 @@ export default class ResourcesComponent {
       tipo: 'Agua',
       medida: 'Litros',
       moneda: '$',
-      descripcion: 'Piso de de rrhh'
+      descripcion: 'Piso de de rrhh',
     },
     {
       id: 2,
       tipo: 'Electricidad',
       medida: 'Kilovatios',
       moneda: '€',
-      descripcion: 'Piso de contabilidad'
+      descripcion: 'Piso de contabilidad',
     },
     {
       id: 3,
       tipo: 'Internet',
       medida: 'Mbps',
       moneda: '£',
-      descripcion: 'Piso de sistemas'
-    }
+      descripcion: 'Piso de sistemas',
+    },
   ];
 
   editResource(event: any) {
@@ -53,7 +52,9 @@ export default class ResourcesComponent {
   }
 
   updateResource(event: any) {
-    const idx = this.resources.findIndex((resource) => resource.id === event.id);
+    const idx = this.resources.findIndex(
+      (resource) => resource.id === event.id
+    );
     if (idx !== -1) {
       this.resources[idx] = event;
       this.resources = [...this.resources]; // <-- This triggers Angular to refresh the view
--- src/app/feature/entities-manager/pages/users/users.component.ts
+++ src/app/feature/entities-manager/pages/users/users.component.ts
@@ -1,15 +1,26 @@
-import { ChangeDetectionStrategy, Component, signal, ChangeDetectorRef} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  signal,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { EnterImgComponent } from '../../../../shared/components/enter-img/enter-img.component';
 import { SubListComponent } from '../../../../shared/components/sub-list/sub-list.component';
 import { ModalComponent } from '../../../../shared/components/modal/modal.component';
 import { JsonFormData } from '../../../../shared/interfaces/form.interface';
 import { JsonPipe } from '@angular/common';
-import { TableCompositionComponent } from "../../../../shared/components/table-composition/table-composition.component";
+import { TableCompositionComponent } from '../../../../shared/components/table-composition/table-composition.component';
 import { UpgratedFormComponent } from '../../../../shared/components/upgrated-form/upgrated-form.component';
 
 @Component({
   selector: 'app-users',
-  imports: [ModalComponent, JsonPipe, TableCompositionComponent, SubListComponent, EnterImgComponent, UpgratedFormComponent],
+  imports: [
+    ModalComponent,
+    TableCompositionComponent,
+    SubListComponent,
+    EnterImgComponent,
+    UpgratedFormComponent,
+  ],
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,10 +33,9 @@ export default class UsersComponent {
   selectedTableUser = signal<any>(undefined);
 
   // atributos de custom properties o cargos
-  atributes:string[]= [];
+  atributes: string[] = [];
 
   onModalFormValues(event: any) {
-
     event['atributes'] = this.atributes;
     this.modalValues.set(event);
     this.onShowModal();
@@ -37,14 +47,14 @@ export default class UsersComponent {
 
   subListOptions(event: Set<string>) {
     this.atributes = [...event];
-    console.log("Lista de opciones", event);
+    console.log('Lista de opciones', event);
   }
 
   onEnterImg(event: string | ArrayBuffer | null) {
     console.log(event);
   }
 
-  onShowTable(){
+  onShowTable() {
     this.showTable = !this.showTable;
   }
 
@@ -73,42 +83,145 @@ export default class UsersComponent {
   updateUser(event: any) {
     //this.usuarios[this.usuarios.findIndex((usuario) => usuario.ID === event.ID)] = event;
     const index = this.usuarios.findIndex((usuario) => usuario.ID === event.ID);
-    console.log("Indice del usuario seleccionado", index);
+    console.log('Indice del usuario seleccionado', index);
     if (index !== -1) {
       this.usuarios[index] = event;
       this.usuarios = [...this.usuarios];
     }
-    
+
     console.log(this.usuarios);
     this.selectedTableUser.set(undefined);
     this.openPopup.update((prev) => !prev);
   }
 
-    usuarios = [
-    {'ID': 1, 'Nombre': 'Juan Pérez', 'Email': 'juan.perez@email.com', 'Activo': 'Sí'},
-    {'ID': 2, 'Nombre': 'María González', 'Email': 'maria.gonzalez@email.com', 'Activo': 'No'},
-    {'ID': 3, 'Nombre': 'Carlos Rodríguez', 'Email': 'carlos.rodriguez@email.com', 'Activo': 'Sí'},
-    {'ID': 4, 'Nombre': 'Ana Martínez', 'Email': 'ana.martinez@email.com', 'Activo': 'Sí'},
-    {'ID': 5, 'Nombre': 'Luis Fernández', 'Email': 'luis.fernandez@email.com', 'Activo': 'No'},
-    {'ID': 6, 'Nombre': 'Sofía López', 'Email': 'sofia.lopez@email.com', 'Activo': 'Sí'},
-    {'ID': 7, 'Nombre': 'Ricardo Jiménez', 'Email': 'ricardo.jimenez@email.com', 'Activo': 'No'},
-    {'ID': 8, 'Nombre': 'Valeria Ortiz', 'Email': 'valeria.ortiz@email.com', 'Activo': 'Sí'},
-    {'ID': 9, 'Nombre': 'Fernando Castro', 'Email': 'fernando.castro@email.com', 'Activo': 'Sí'},
-    {'ID': 10, 'Nombre': 'Patricia Rivas', 'Email': 'patricia.rivas@email.com', 'Activo': 'No'},
-    {'ID': 11, 'Nombre': 'Gabriel Muñoz', 'Email': 'gabriel.munoz@email.com', 'Activo': 'Sí'},
-    {'ID': 12, 'Nombre': 'Andrea Silva', 'Email': 'andrea.silva@email.com', 'Activo': 'Sí'},
-    {'ID': 13, 'Nombre': 'Diego Vargas', 'Email': 'diego.vargas@email.com', 'Activo': 'No'},
-    {'ID': 14, 'Nombre': 'Lorena Castillo', 'Email': 'lorena.castillo@email.com', 'Activo': 'Sí'},
-    {'ID': 15, 'Nombre': 'Héctor Navarro', 'Email': 'hector.navarro@email.com', 'Activo': 'No'},
-    {'ID': 16, 'Nombre': 'Camila Herrera', 'Email': 'camila.herrera@email.com', 'Activo': 'Sí'},
-    {'ID': 17, 'Nombre': 'Marco Salazar', 'Email': 'marco.salazar@email.com', 'Activo': 'Sí'},
-    {'ID': 18, 'Nombre': 'Isabel Fuentes', 'Email': 'isabel.fuentes@email.com', 'Activo': 'No'},
-    {'ID': 19, 'Nombre': 'Rodrigo Mendoza', 'Email': 'rodrigo.mendoza@email.com', 'Activo': 'Sí'},
-    {'ID': 20, 'Nombre': 'Cristina Paredes', 'Email': 'cristina.paredes@email.com', 'Activo': 'Sí'},
-    {'ID': 21, 'Nombre': 'Cristina Paredes', 'Email': 'cristina.paredes@email.com', 'Activo': 'Sí', 'Ciudad': 'Cuenca'},
-
-  ]
-  constructor(private cdr: ChangeDetectorRef){
-
-  }
+  usuarios = [
+    {
+      ID: 1,
+      Nombre: 'Juan Pérez',
+      Email: 'juan.perez@email.com',
+      Activo: 'Sí',
+    },
+    {
+      ID: 2,
+      Nombre: 'María González',
+      Email: 'maria.gonzalez@email.com',
+      Activo: 'No',
+    },
+    {
+      ID: 3,
+      Nombre: 'Carlos Rodríguez',
+      Email: 'carlos.rodriguez@email.com',
+      Activo: 'Sí',
+    },
+    {
+      ID: 4,
+      Nombre: 'Ana Martínez',
+      Email: 'ana.martinez@email.com',
+      Activo: 'Sí',
+    },
+    {
+      ID: 5,
+      Nombre: 'Luis Fernández',
+      Email: 'luis.fernandez@email.com',
+      Activo: 'No',
+    },
+    {
+      ID: 6,
+      Nombre: 'Sofía López',
+      Email: 'sofia.lopez@email.com',
+      Activo: 'Sí',
+    },
+    {
+      ID: 7,
+      Nombre: 'Ricardo Jiménez',
+      Email: 'ricardo.jimenez@email.com',
+      Activo: 'No',
+    },
+    {
+      ID: 8,
+      Nombre: 'Valeria Ortiz',
+      Email: 'valeria.ortiz@email.com',
+      Activo: 'Sí',
+    },
+    {
+      ID: 9,
+      Nombre: 'Fernando Castro',
+      Email: 'fernando.castro@email.com',
+      Activo: 'Sí',
+    },
+    {
+      ID: 10,
+      Nombre: 'Patricia Rivas',
+      Email: 'patricia.rivas@email.com',
+      Activo: 'No',
+    },
+    {
+      ID: 11,
+      Nombre: 'Gabriel Muñoz',
+      Email: 'gabriel.munoz@email.com',
+      Activo: 'Sí',
+    },
+    {
+      ID: 12,
+      Nombre: 'Andrea Silva',
+      Email: 'andrea.silva@email.com',
+      Activo: 'Sí',
+    },
+    {
+      ID: 13,
+      Nombre: 'Diego Vargas',
+      Email: 'diego.vargas@email.com',
+      Activo: 'No',
+    },
+    {
+      ID: 14,
+      Nombre: 'Lorena Castillo',
+      Email: 'lorena.castillo@email.com',
+      Activo: 'Sí',
+    },
+    {
+      ID: 15,
+      Nombre: 'Héctor Navarro',
+      Email: 'hector.navarro@email.com',
+      Activo: 'No',
+    },
+    {
+      ID: 16,
+      Nombre: 'Camila Herrera',
+      Email: 'camila.herrera@email.com',
+      Activo: 'Sí',
+    },
+    {
+      ID: 17,
+      Nombre: 'Marco Salazar',
+      Email: 'marco.salazar@email.com',
+      Activo: 'Sí',
+    },
+    {
+      ID: 18,
+      Nombre: 'Isabel Fuentes',
+      Email: 'isabel.fuentes@email.com',
+      Activo: 'No',
+    },
+    {
+      ID: 19,
+      Nombre: 'Rodrigo Mendoza',
+      Email: 'rodrigo.mendoza@email.com',
+      Activo: 'Sí',
+    },
+    {
+      ID: 20,
+      Nombre: 'Cristina Paredes',
+      Email: 'cristina.paredes@email.com',
+      Activo: 'Sí',
+    },
+    {
+      ID: 21,
+      Nombre: 'Cristina Paredes',
+      Email: 'cristina.paredes@email.com',
+      Activo: 'Sí',
+      Ciudad: 'Cuenca',
+    },
+  ];
+  constructor(private cdr: ChangeDetectorRef) {}
 }
